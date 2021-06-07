6c4b492bb021974aed44d28ceee5efee84c5c1bd
yemaney <yemane_@outlook.com>
2021-06-06 23:42:07 -0400

    added script to import csv and xls data

---
+++ edalit/file_importer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+class File:
+    Data = None
+    
+    def __init__(self, file) -> None:
+        self.file = file
+    
+    def import_csv(self, sep: str):
+        return pd.read_csv(self.file, sep)
+    
+    def xls_sheets(self):
+        return pd.ExcelFile(self.file).sheet_names
+    
+    def import_xls(self, sheet_name):
+        return pd.ExcelFile(self.file).parse(sheet_name=sheet_name)
